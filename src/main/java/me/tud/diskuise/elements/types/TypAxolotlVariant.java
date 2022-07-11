2597ee658516cd8d038bf5197474f0aea1469ed5
UnderscoreTud <mmbakkar06@gmail.com>
2022-07-11 03:09:30 +0300

    ⚒️Fixed Axolotl Variant type registering as Painting Art

--- src/main/java/me/tud/diskuise/elements/types/TypAxolotlVariant.java
+++ src/main/java/me/tud/diskuise/elements/types/TypAxolotlVariant.java
@@ -7,7 +7,7 @@ import org.bukkit.entity.Axolotl;
 public class TypAxolotlVariant {
 
     static {
-        ClassUtils.registerClass(new EnumClassInfo<>(Axolotl.Variant.class, "paintingart")
+        ClassUtils.registerClass(new EnumClassInfo<>(Axolotl.Variant.class, "axolotlvariant")
                 .user("axolotl ?variants?")
                 .name("Axolotl Variant")
                 .description("Represents the variant of an axolotl")
