f2fd2d59d9a2d59de9570f0414e6264887b20eb1
UnderscoreTud <mmbakkar06@gmail.com>
2022-07-16 22:03:24 +0300

    Docs for CondHasArms

--- src/main/java/me/tud/diskuise/elements/entities/armorstand/conditions/CondHasArms.java
+++ src/main/java/me/tud/diskuise/elements/entities/armorstand/conditions/CondHasArms.java
@@ -1,8 +1,14 @@
 package me.tud.diskuise.elements.entities.armorstand.conditions;
 
+import ch.njol.skript.doc.*;
 import me.tud.diskuise.elements.entities.armorstand.BetterArmorStandWatcher;
 import me.tud.diskuise.util.skript.WatcherPropertyCondition;
 
+@Name("Armor Stand Disguise - Has Arms")
+@Description("Checks whether an armor stand disguise has its arms shown")
+@Examples("show the arms of player's disguise")
+@Since("INSERT VERSION")
+@RequiredPlugins("LibsDisguises")
 public class CondHasArms extends WatcherPropertyCondition<BetterArmorStandWatcher> {
 
     static {
