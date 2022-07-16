2eabf6bd8c7ae7c8f2676159e549056ae9a9d949
UnderscoreTud <mmbakkar06@gmail.com>
2022-07-16 23:05:33 +0300

    Added Armor Stand 'Marker' elements

---
+++ src/main/java/me/tud/diskuise/elements/entities/armorstand/effects/EffMakeMarker.java
@@ -0,0 +1,29 @@
+package me.tud.diskuise.elements.entities.armorstand.effects;
+
+import ch.njol.skript.doc.*;
+import me.libraryaddict.disguise.disguisetypes.watchers.ArmorStandWatcher;
+import me.tud.diskuise.util.skript.WatcherMakeEffect;
+import org.bukkit.event.Event;
+
+@Name("Armor Stand Disguise - Make Marker")
+@Description("Set whether an armor stand disguise is a marker")
+@Examples("make player's disguise a marker")
+@Since("INSERT VERSION")
+@RequiredPlugins("LibsDisguises")
+public class EffMakeMarker extends WatcherMakeEffect<ArmorStandWatcher> {
+
+    static {
+        register(EffMakeMarker.class, "[:not] [a] marker");
+    }
+
+    @Override
+    protected String getProperty() {
+        return "marker";
+    }
+
+    @Override
+    protected void make(Event e, ArmorStandWatcher armorStandWatcher) {
+        armorStandWatcher.setMarker(!isNegated());
+    }
+
+}
