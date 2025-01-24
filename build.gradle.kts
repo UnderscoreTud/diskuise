46b74ab5de391bde5512fe584f49f70272564d9a
_tud <98935832+UnderscoreTud@users.noreply.github.com>
2025-01-24 15:49:00 +0300

    Switch to Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,64 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import java.util.*
+
+plugins {
+    id("java")
+    id("com.gradleup.shadow") version "8.3.0"
+}
+
+group = "me.tud"
+version = "0.3.4"
+description = "Diskuise"
+java.sourceCompatibility = JavaVersion.VERSION_21
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+    maven {
+        url = uri("https://oss.sonatype.org/content/groups/public/")
+    }
+    maven {
+        url = uri("https://repo.skriptlang.org/releases")
+    }
+    maven {
+        url = uri("https://repo.md-5.net/content/groups/public")
+    }
+    maven {
+        url = uri("https://repo.maven.apache.org/maven2/")
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+    compileOnly("com.github.SkriptLang:Skript:2.10.0")
+    compileOnly("LibsDisguises:LibsDisguises:10.0.44")
+    compileOnly("org.eclipse.jdt:org.eclipse.jdt.annotation:2.2.600")
+}
+
+val properties = Properties()
+val localProperties = file("local.properties")
+if (localProperties.isFile)
+    properties.load(localProperties.inputStream())
+val jarName: String? = properties.getProperty("jarName")
+val jarDir: String? = properties.getProperty("jarDir")
+
+tasks.withType<Jar> {
+    archiveFileName.set(jarName ?: "${project.name}-${project.version}.jar")
+    if (jarDir != null)
+        destinationDirectory.set(project.file(jarDir))
+}
+
+tasks.withType<ShadowJar> {
+    relocate("org.bstats", "me.tud.diskuise.bstats")
+}
+
+tasks.processResources {
+    expand(mapOf(
+        "name" to project.name,
+        "version" to project.version,
+        "group" to project.group
+    ))
+    filteringCharset = "UTF-8"
+}
